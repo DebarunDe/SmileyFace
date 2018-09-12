400837874d78c7bdba3e88d4d3ddfc2a3768a442
DebarunDe <42978582+DebarunDe@users.noreply.github.com>
2018-09-12 14:25:26 -0400

    Add files via upload

---
+++ src/smileyFace/SmileyFace.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author DebarunDe
+ * SmileyFace Period. 7
+ * 
+ */
+
+package smileyFace;
+
+public class SmileyFace {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\\\\\\\\\\\");
+		System.out.println("| * * |");
+		System.out.println("|  *  |");
+		System.out.println("| \\_/ |");
+		System.out.println("\\_____/");
+	}
+
+}
